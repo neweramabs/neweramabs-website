130f2b456fc74e3f29642f968bada8dd33a7507f
leifhanack <leif.hanack@gmail.com>
2024-06-07 12:59:01 -0700

    add initial content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,js}",
-    "./node_modules/flowbite/**/*.js"
-  ],
+  content: ['./src/**/*.{html,js}', './node_modules/flowbite/**/*.js'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        nem: {
+          50: '#F7FEE7',
+          100: '#DBF8B1',
+          200: '#C1F27C',
+          300: '#A6EC48',
+          400: '#8CE614',
+          light: '#8ce614' /* nem-400 */,
+          500: '#62be0d',
+          600: '#3a9707',
+          700: '#117000',
+          dark: '#117000' /* nem-700 */,
+          800: '#145a02',
+          900: '#174403',
+          950: '#1A2E05',
+        },
+      },
+    },
   },
-  plugins: [
-    require('flowbite/plugin')
-  ]
-}
-
+  plugins: [require('flowbite/plugin'), require('tailwindcss-aria-attributes')],
+};
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: './src',  
+  root: './src',
   build: {
-    outDir: '../dist',  
-    emptyOutDir: true,  
-  }
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  server: {
+    host: true,
+    port: 5173,
+    open: false,
+    cors: true,
+  },
 });
