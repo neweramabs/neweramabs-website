c178ceb7a394a6216a627415cc00e08f53bfeda7
leifhanack <leif.hanack@gmail.com>
2024-06-08 23:34:58 -0700

    fix vite multi page prod setup

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
   root: './src',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './src/index.html',
+        science: './src/science.html',
+        diseaseX: './src/disease-x.html',
+        service: './src/service.html',
+        products: './src/products.html',
+        about: './src/about.html',
+        privacyPolicy: './src/privacy-policy.html',
+        termsAndConditions: './src/terms-and-conditions.html',
+        legalDisclosure: './src/legal-disclosure.html',
+      },
+    },
   },
   server: {
     host: true,
@@ -12,4 +26,5 @@ export default defineConfig({
     open: false,
     cors: true,
   },
+  plugins: [createHtmlPlugin()],
 });
